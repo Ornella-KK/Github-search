1df6ea805e28d73c7ce1131bc38d7806dd481e71
Ornella-KK <ornellakankazi@gmail.com>
2020-11-17 20:09:48 +0200

    added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AboutComponent } from './about/about.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    AboutComponent
+    AboutComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
