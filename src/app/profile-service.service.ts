898c58e608f74e0f7724dc4b804a1f72370cee92
Ornella-KK <ornellakankazi@gmail.com>
2020-11-17 20:05:02 +0200

    added changes

--- src/app/profile-service.service.ts
+++ src/app/profile-service.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user'
+import { Repository } from './repository'
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProfileServiceService {
+export class ProfileService {
+  userProfile: User
 
-  constructor() { }
+  userRepo: Repository
+  constructor(private http: HttpClient) {
+    this.userProfile = new User('', '', 0, 0, '', '', '' );
+
+  }
+
+  searchUser(user) {
+    interface Responsee {
+      login: string,
+      avatar_url: string,
+      followers: number,
+      following: number,
+      public_repos: string,
+      html_url : any,
+      email :any
+
+
+
+    }
+
+    let mainUrl = environment.API_URL + user + '?access_token=' + environment.API_KEY;
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<Responsee>(mainUrl).toPromise().then(res => {
+        this.userProfile = res;
+        resolve()
+
+      }, error => {
+        reject();
+      })
+
+    });
+    return promise
+  }
+
+  displayRepos(user) {
+    interface apiResponse {
+      name: string,
+      description: string,
+      
+
+    }
+
+    let repoUrl = environment.API_URL + user + '/repos' + '?access_token=' + environment.API_KEY;
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<apiResponse>(repoUrl).toPromise().then(response => {
+        this.userRepo = response;
+        resolve()
+
+      }, error => {
+        reject();
+      })
+
+    });
+    return promise
+  }
+
+
 }
+
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile-service.service';
+import { User } from '../user';
+import { Repository } from '../repository';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  userProfile: User
+  userRepo: Repository
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private profileService: ProfileService) {
+ 
   }
 
+  searchUser(user) {
+
+    this.profileService.searchUser(user).then((done) => {
+      this.userProfile = this.profileService.userProfile;
+    },
+      (error) => {
+        console.log(error)
+      }
+    )
+
+    this.profileService.displayRepos(user).then((done) => {
+      this.userRepo = this.profileService.userRepo;
+    },
+      (error) => {
+        console.log(error)
+      }
+    )
+
+  }
+
+  ngOnInit(): void {
+
+    this.searchUser('Ornella-KK');
+  }
 }
